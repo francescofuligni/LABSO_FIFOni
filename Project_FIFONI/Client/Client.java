6d247601086c7bf004b13e1ffe02dcd2c7218b5d
Roberto Zanolli <roberto.zanolli.2003@gmail.com>
2024-09-28 18:56:25 +0200

    Sistemati Main di client e server per accettare parametri da terminale
    Sistemati Main di client e server per accettare parametri da terminale + controlli per verificare che sia rispettato il formato.

--- Project_FIFONI/Client/Client.java
+++ Project_FIFONI/Client/Client.java
@@ -6,69 +6,87 @@ import java.util.Scanner;
 public class Client {
 
     public static void main(String[] args) {
-        final String serverAddress = "localhost";   // Indirizzo del server (usare l'IP se non è locale)
-        final int serverPort = 9000;                // Porta del server
 
-        try (Socket socket = new Socket(serverAddress, serverPort);
-             PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
-             Scanner fromServer = new Scanner(socket.getInputStream());
-             Scanner userInput = new Scanner(System.in)) {
+
+        if (args.length != 2) {
+            System.err.println("Errore di sintassi: utilizzare java Client <indirizzo> <porta>");
+        }
+        else{
+
+            final String serverAddress = args[0];   // Indirizzo del server (es. localhost o IP)
+            final int serverPort;
+    
+            try {
+                // Parsing della porta del server
+                serverPort = Integer.parseInt(args[1]);
             
-            // Ricevere il primo messaggio dal server
-            String serverMessage = fromServer.nextLine();
-            System.out.println("Messaggio dal server: " + serverMessage);
-
-            
-
-
-            // Primo comando per registrazione
-            String message = "";
-            boolean unknown = true;     // Flag per comandi sconosciuti
-
-            // IDEA STRUTTURA:
-            do {
-                System.out.print("Inserire comando 'publisher'/'subscriber'/'show'/'quit': ");
-                message = userInput.nextLine();
-                
-                if(message.startsWith("publisher")) {
-                    // Registra il client come publisher sul topic
-                    System.out.println("Registrato correttamente come publisher");
-                    unknown = false;
-                } else if(message.startsWith("subscriber")) {
-                    // Registra il client come subscriber sul topic
-                    System.out.println("Registrato correttamente come subscriber");
-                    unknown = false;
-                } else if(message.equals("show")) {
-                    // Mostra la lista di tutti i topic creati dai publisher
-                    System.out.println("Lista dei topic creati: empty");
-                    unknown = false;
-                } else {
-                    // Riprova: comando sconosciuto
-                    System.out.println("Comando sconoscuto, riprovare");
+                try (Socket socket = new Socket(serverAddress, serverPort);
+                    PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
+                    Scanner fromServer = new Scanner(socket.getInputStream());
+                    Scanner userInput = new Scanner(System.in)) {
+                    
+                    // Ricevere il primo messaggio dal server
+                    String serverMessage = fromServer.nextLine();
+                    System.out.println("Messaggio dal server: " + serverMessage);
+        
+                    
+        
+        
+                    // Primo comando per registrazione
+                    String message = "";
+                    boolean unknown = true;     // Flag per comandi sconosciuti
+        
+                    // IDEA STRUTTURA:
+                    do {
+                        System.out.print("Inserire comando 'publisher'/'subscriber'/'show'/'quit': ");
+                        message = userInput.nextLine();
+                        
+                        if(message.startsWith("publisher")) {
+                            // Registra il client come publisher sul topic
+                            System.out.println("Registrato correttamente come publisher");
+                            unknown = false;
+                        } else if(message.startsWith("subscriber")) {
+                            // Registra il client come subscriber sul topic
+                            System.out.println("Registrato correttamente come subscriber");
+                            unknown = false;
+                        } else if(message.equals("show")) {
+                            // Mostra la lista di tutti i topic creati dai publisher
+                            System.out.println("Lista dei topic creati: empty");
+                            unknown = false;
+                        } else {
+                            // Riprova: comando sconosciuto
+                            System.out.println("Comando sconoscuto, riprovare");
+                        }
+        
+                    } while(unknown);   // Riprova se il comando è sconosciuto
+        
+        
+                    while(!message.equals("quit")) {    // Finché non decide di terminare la connessione
+                        // Richiede altri comandi
+                        System.out.print("Nuovo comando: ");
+                        message = userInput.nextLine();
+                        toServer.println(message);
+        
+                        // Risposta dal server (echo)
+                        String serverResponse = fromServer.nextLine();
+                        System.out.println("Echo dal server: " + serverResponse);
+                    }
+                    System.out.println("Client arrestato");
+        
+        
+        
+        
+        
+                } catch (IOException e) {
+                    System.err.println("Errore durante la connessione al server");
+                    e.printStackTrace();
                 }
 
-            } while(unknown);   // Riprova se il comando è sconosciuto
-
-
-            while(!message.equals("quit")) {    // Finché non decide di terminare la connessione
-                // Richiede altri comandi
-                System.out.print("Nuovo comando: ");
-                message = userInput.nextLine();
-                toServer.println(message);
-
-                // Risposta dal server (echo)
-                String serverResponse = fromServer.nextLine();
-                System.out.println("Echo dal server: " + serverResponse);
+            } catch (NumberFormatException e) {
+                System.err.println("Errore: la porta deve essere un numero intero.");
+                e.printStackTrace();
             }
-            System.out.println("Client arrestato");
-
-
-
-
-
-        } catch (IOException e) {
-            System.err.println("Errore durante la connessione al server");
-            e.printStackTrace();
         }
-    }
+       
 }
+    }
--- Project_FIFONI/Server/Server.java
+++ Project_FIFONI/Server/Server.java
@@ -7,48 +7,66 @@ import java.util.Scanner;
 public class Server {
 
     public static void main(String[] args) {
-        final int port = 9000;
 
-        // Dichiarazione del ServerSocket
-        try (ServerSocket server = new ServerSocket(port)) {
-            System.out.println("Server in ascolto sulla porta " + port);
+        if (args.length != 1) {
+            System.err.println("Errore di sintassi: utilizzare java Server <porta>");
+        } else {
 
-            // Attendere una connessione da parte di un client
-            Socket s = server.accept();
-            System.out.println("Connessione accettata da " + s.getInetAddress());
+            try {
+                // Parsing dell'argomento in intero
+                final int port = Integer.parseInt(args[0]);
 
-            // Usa try-with-resources per garantire che le risorse siano chiuse automaticamente
-            try (Scanner from = new Scanner(s.getInputStream());
-                 PrintWriter to = new PrintWriter(s.getOutputStream(), true)) {
+                if (port <= 1024) {
+                    System.err.println("Errore: la porta deve essere maggiore di 1024.");
+                    System.exit(1);
+                }
 
-                // Inviare una richiesta al client
-                to.println("connessione accettata");
+                // Dichiarazione del ServerSocket
+                try (ServerSocket server = new ServerSocket(port)) {
+                    System.out.println("Server in ascolto sulla porta " + port);
 
+                    // Attendere una connessione da parte di un client
+                    Socket s = server.accept();
+                    System.out.println("Connessione accettata da " + s.getInetAddress());
 
-                // IDEA STRUTTURA:
-                String response = "";
-                do {
-                    // Ascolta i comandi del client
-                    response = from.nextLine();
-                    System.out.println("Ricevuto dal client: " + response);
-                    
-                    // echo
-                    to.println(response);
-                } while(!response.equals("quit"));
-                
-                System.out.println("Server arrestato");
-                
-            } catch (IOException e) {
-                System.err.println("Errore durante la comunicazione con il client");
+                    // Usa try-with-resources per garantire che le risorse siano chiuse
+                    // automaticamente
+                    try (Scanner from = new Scanner(s.getInputStream());
+                            PrintWriter to = new PrintWriter(s.getOutputStream(), true)) {
+
+                        // Inviare una richiesta al client
+                        to.println("connessione accettata");
+
+                        // IDEA STRUTTURA:
+                        String response = "";
+                        do {
+                            // Ascolta i comandi del client
+                            response = from.nextLine();
+                            System.out.println("Ricevuto dal client: " + response);
+
+                            // echo
+                            to.println(response);
+                        } while (!response.equals("quit"));
+
+                        System.out.println("Server arrestato");
+
+                    } catch (IOException e) {
+                        System.err.println("Errore durante la comunicazione con il client");
+                        e.printStackTrace();
+                    }
+
+                    // Chiudere il socket
+                    s.close();
+
+                } catch (IOException e) {
+                    System.err.println("Errore durante la creazione del server socket sulla porta " + port);
+                    e.printStackTrace();
+                }
+
+            } catch (NumberFormatException e) {
+                System.err.println("Errore: la porta deve essere un numero intero.");
                 e.printStackTrace();
             }
-
-            // Chiudere il socket
-            s.close();
-
-        } catch (IOException e) {
-            System.err.println("Errore durante la creazione del server socket sulla porta " + port);
-            e.printStackTrace();
         }
     }
 }
